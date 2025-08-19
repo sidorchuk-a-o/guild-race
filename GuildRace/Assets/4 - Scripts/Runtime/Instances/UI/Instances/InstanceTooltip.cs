e7885c1406f8a6745d42a66eebcd430863f8ba87
Alexander Sidorchuk <a.o.sidorchuk@gmail.com>
2025-08-19 19:50:49 +0300

    add: тултип для инстов в списке

---
+++ GuildRace/Assets/4 - Scripts/Runtime/Instances/UI/Instances/InstanceTooltip.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading;
+using Cysharp.Threading.Tasks;
+using AD.Services.Router;
+using AD.ToolsCollection;
+using AD.UI;
+using UnityEngine;
+using Game.UI;
+
+namespace Game.Instances
+{
+    public class InstanceTooltip : TooltipContainer
+    {
+        [SerializeField] private UIText nameText;
+        [SerializeField] private UIText descText;
+
+        public override UniTask Init(ViewModel viewModel, CompositeDisp disp, CancellationTokenSource ct)
+        {
+            var instanceVM = viewModel as InstanceVM;
+
+            nameText.SetTextParams(instanceVM.NameKey);
+            descText.SetTextParams(instanceVM.DescKey);
+
+            return UniTask.CompletedTask;
+        }
+    }
+}
