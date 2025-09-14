daade775f14a1c3fe7a88a6b3721935bcbf7d8d4
Alexander Sidorchuk <a.o.sidorchuk@gmail.com>
2025-09-14 20:19:47 +0300

    add: аналитика уровней гильдии

---
+++ GuildRace/Assets/4 - Scripts/Runtime/GuildLevels/Service/Analytics/GuildLevelsAnalyticsExtensions.cs
@@ -0,0 +1,18 @@
+﻿using AD.Services.Analytics;
+using AD.Services.Store;
+
+namespace Game.GuildLevels
+{
+    public static class GuildLevelsAnalyticsExtensions
+    {
+        public static void UnlockLevel(this IAnalyticsService analytics, LevelInfo level)
+        {
+            var parameters = AnalyticsParams.Default;
+            parameters.AddCurrencyAmount(level.UnlockPrice.Key);
+            parameters["price"] = level.UnlockPrice.Value.ToString();
+            parameters["level"] = level.Level;
+
+            analytics?.SendEvent("unlock_guild_level", parameters);
+        }
+    }
+}
