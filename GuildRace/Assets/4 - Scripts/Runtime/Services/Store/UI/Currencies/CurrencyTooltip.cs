2acd5faf69c656df79c224f73ea3d006b7f1425d
Alexander Sidorchuk <a.o.sidorchuk@gmail.com>
2025-09-25 20:25:42 +0300

    add: тултип для валюты в заголовке

---
+++ GuildRace/Assets/4 - Scripts/Runtime/Services/Store/UI/Currencies/CurrencyTooltip.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading;
+using AD.Services.Router;
+using AD.Services.Store;
+using AD.ToolsCollection;
+using AD.UI;
+using Cysharp.Threading.Tasks;
+using Game.UI;
+using UnityEngine;
+
+namespace Game.Store
+{
+    public class CurrencyTooltip : TooltipContainer
+    {
+        [SerializeField] private UIText amountText;
+
+        public override UniTask Init(ViewModel viewModel, CompositeDisp disp, CancellationTokenSource ct)
+        {
+            var currencyVM = viewModel as CurrencyVM;
+            var amountStr = currencyVM.Value.Value.ToString();
+
+            amountText.SetTextParams(amountStr);
+
+            return UniTask.CompletedTask;
+        }
+    }
+}
